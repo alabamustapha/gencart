e916097573b76424bcd24ccd592c6a1fab1b0e5a
= <debowale01@gmail.com>
2017-12-13 03:37:52 +0100

    initial commit on branch index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,91 +5,165 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>GenCart | Home</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <!-- Bootstrap maxcdn -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        
+        <!-- Custom stylesheet - for your changes -->
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <div class="content">
+            <div class="jumbotron jumbotron-fluid">
+                <a href="#" class="btn btn-success float-right">Login</a>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <div class="container">                    
+                    <div class="signup-widget round shadow">
+                        <div class="logo">
+                            <h1>LOGO</h1>
+                        </div>
+                        <div class="sign-up">
+                            <div class="gencart">
+                               <div class="title"> Get your groceries delivered from local stores</div>
+                                <form class="signup-form" method="">
+                                    <div class="form-group row">
+                                        <div class="col-lg-10">
+                                            <input type="text" class="form-control form-control-lg input-zip" placeholder="Enter Zip Code">
+                                        </div>
+                                        <div class="col-lg-10">
+                                            <button type="button" class="btn btn-success btn-lg btn-block">Continue</button>
+                                        </div>
+                                    </div>
+                                </form> 
+                                
+                                <div class="signup-or-login">
+                                    <ul>
+                                        <li>already have an account? <a href="#">Log in</a></li>
+                                    </ul>
+                                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+
+
+                                <div class="free-message">
+                                    <i class="ic-icon ic-icon-gift"></i>
+                                    <span class="bold">FREE</span>
+                                     delivery on your first order*
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div> <!-- end container -->
             </div>
-        </div>
+
+            <!-- grid section -->
+
+            <section class=" section how-it-works">
+                <div class="section-wrap">
+                    <div class="container">
+
+                        <div class="row">
+
+                            <div class="col-lg-4">
+                                <div class="img">
+                                    <img src="{{ asset('images/product.jpg')}}" alt="">
+                                </div>
+
+                                <h3>Products You Love</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                </p>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <div class="img">
+                                    <img src="{{ asset('images/sameday.jpg')}}" alt="">
+                                </div>
+
+                                <h3>Products You Love</h3>
+                                <p >
+                                    Lorem ipsum dolor sit amet consectetur
+                                </p>
+                            </div>
+
+                            <div class="col-lg-4">
+                                <div class="img">
+                                    <img src="{{ asset('images/time.jpg')}}" alt="">
+                                </div>
+
+                                <h3>Products You Love</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                </p>
+                            </div>
+
+                            
+                            
+                            
+                        </div> <!-- end row -->
+
+                    </div>
+                </div>
+            </section>
+
+            <section class="deals">
+                <div class="section-wrap">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8">
+                                <img src="{{ asset('images/coupon.jpg')}}" alt="" srcset="">
+                            </div>
+                            <div class="col-lg-4 mgt-60">
+                                <h3>Deals that delights </h3>
+
+                                <p>Saving money on Instacart is easy. Find exclusive coupons on hundreds of items</p>
+
+                                <p>
+                                    <button type="submit" class="btn btn-success float-left cta">Start Saving</button>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="section browse-product">
+
+                <div class="section-wrap">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h3>Deals that delights </h3>
+                                <p>Saving money on Instacart is easy. Find exclusive coupons on hundreds of items</p>
+
+                                 <p>
+                                    <button type="submit" class="btn btn-success float-left cta">Start Saving</button>
+                                </p>
+                            </div>
+                            <div class="col-lg-8">    
+                                <img src="{{ asset('images/coupon.jpg')}}" alt="" srcset="">  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+        </div> <!-- end content -->
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/bootstrap.min.js')}}"></script>
     </body>
 </html>
