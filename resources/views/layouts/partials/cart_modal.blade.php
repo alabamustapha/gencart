f56678a93d276410b2de4ca59194c112622c4cba
= <debowale01@gmail.com>
2017-12-31 10:45:12 +0100

    cart modal

--- resources/views/layouts/partials/cart_modal.blade.php
+++ resources/views/layouts/partials/cart_modal.blade.php
@@ -99,6 +99,55 @@
                         </div>
 
                     </div>
+
+                    <div class="ibox-content">
+                        <div class="table-responsive">
+                            <table class="table shoping-cart-table">
+
+                                <tbody>
+                                <tr>
+                                    <td width="90">
+                                        <div class="cart-product-imitation">
+                                        </div>
+                                    </td>
+                                    <td class="desc">
+                                        <h3>
+                                            <a href="#" class="text-navy">
+                                                PM software
+                                            </a>
+                                        </h3>
+                                        <p class="small">
+                                            Readable content of a page when looking at its layout.
+                                        </p>
+                                        <!-- <dl class="small m-b-none">
+                                            <dt>Description lists</dt>
+                                            <dd>A description list is perfect for defining terms.</dd>
+                                        </dl> -->
+
+                                        <div class="m-t-sm">
+                                            <a href="#" class="text-muted"><i class="fa fa-edit"></i> instructions</a>
+                                            |
+                                            <a href="#" class="text-muted"><i class="fa fa-trash"></i> Remove</a>
+                                        </div>
+                                    </td>
+
+                                    <!-- <td>
+                                        $1.90
+                                    </td> -->
+                                    <td width="65">
+                                        <input type="text" class="form-control" placeholder="1">
+                                    </td>
+
+                                    <td>
+                                        $1.9
+                                    </td>
+
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
                     <div class="ibox-content">
                         <div class="table-responsive">
                             <table class="table shoping-cart-table">
